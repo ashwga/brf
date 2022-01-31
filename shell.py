b3086314da3d47184cee7b0a6739121e9d36457e
ashwga <xomjak2006@gmail.com>
2022-01-31 14:37:45 +0300

    Shell qol improvements

--- shell.py
+++ shell.py
@@ -8,11 +8,14 @@ print("Input empty line for stack")
 try:
     while 1:
         user_input = input("\n> ") + " "
-        if user_input.strip() != "":
-            tokens = brf.preprocess(user_input)
-            brf.exec_tokens(tokens, stack, symbols, False)
-        else:
-            print(f"{', '.join(str(i) for i in stack)}")
+        try:
+            if user_input.strip() != "":
+                tokens = brf.preprocess(user_input)
+                brf.exec_tokens(tokens, stack, symbols, False)
+            else:
+                print(f"{', '.join(str(i) for i in stack)}")
+        except KeyboardInterrupt:
+            pass
 
 except (KeyboardInterrupt, EOFError):
     print("bye!")
